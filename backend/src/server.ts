9f523a721d38a74ee8bc66a147f1105aa7e9d2f7
fatratra-png <nyfatratrar@gmail.com>
2026-08-10 09:35:03 +0300

    feat(server): start express app and listen on configured port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,1 +1,10 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config();
+
+const PORT: number = Number(process.env.PORT);
+
+app.listen(PORT, () => {
+  console.log(`Server is launched on http://localhost${PORT}`);
+});
