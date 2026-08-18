dfcae00c97b75d140daf0ad4eb01f6a392b3cba1
fatratra-png <nyfatratrar@gmail.com>
2026-08-18 19:49:03 +0300

    chore:reduce the JWT time to live to 1h after tests

--- backend/src/security/jwt.ts
+++ backend/src/security/jwt.ts
@@ -4,7 +4,7 @@ import type { PublicUser } from "../types/user.types.js";
 const JWT_SECRET = process.env.JWT_SECRET!;
 
 export const signToken = (user: PublicUser): string =>
-  jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: "1d" });
+  jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, { expiresIn: "1h" });
 
 export const verifyToken = (token: string): PublicUser =>
   jwt.verify(token, JWT_SECRET) as PublicUser;
